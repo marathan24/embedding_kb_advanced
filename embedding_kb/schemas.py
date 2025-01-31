7e419e8d95bc664abd6126857f35fd2eee82e8d9
Ashwarya Maratha <marathaashwarya@gmail.com>
2025-01-31 17:48:47 +0000

    semchunk integrated

--- embedding_kb/schemas.py
+++ embedding_kb/schemas.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel
-from typing import Literal, Optional, Dict, Any
-from naptha_sdk.schemas import KBConfig
+# /embedding_kb/schemas.py
+
+from pydantic import BaseModel, Field
+from typing import Literal, Optional, Dict, Any, List
 
 class InputSchema(BaseModel):
     func_name: Literal["init", "run_query", "add_data"]
@@ -10,3 +11,15 @@ class RetrieverConfig(BaseModel):
     type: str = "vector"
     field: str = "embedding" 
     k: int = 5
+
+class LLMConfigOptions(BaseModel):
+    chunk_size: int = 2000
+    chunk_overlap: int = 200
+    separators: List[str] = ["\n\n", "\n", ". ", " ", ""]
+    embedding_dim: int = 1536
+
+class LLMConfig(BaseModel):
+    config_name: str
+    client: str
+    model: str
+    options: LLMConfigOptions
